5385b1092e27292688689f6f99d3df0d666f5f40
xiaoqinxing <1415289814@qq.com>
2021-10-28 00:29:03 +0800

    ADD opencv

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+mkdir -p ./build
 cmake --build ./build --config Release --target all -- -j 18
+
+case "$1" in
+    --install)  echo "start install"
+    pushd ./build
+    make install
+    popd;;
+esac
